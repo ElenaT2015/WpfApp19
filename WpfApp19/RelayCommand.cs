dde4a9226241e4fe623f510c2e5a1ab47abfaa82
Tsygankovae.a <Tsygankovae.a@outlook.com>
2022-02-14 22:26:00 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19.Models;
+
+namespace WpfApp19
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public RelayCommand(Func<object, bool> canExecute)
+        {
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> Execute, Func<object, bool> CanExecte = null, Func<object, bool> canExecute = null)
+        {
+            execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            canExecute = CanExecute;
+        }
+        public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => execute(parameter);  
+        
+    }
+}
+
---
+++ WpfApp19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp19.Models;
+
+namespace WpfApp19.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string ProprtyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(ProprtyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CalcCommand { get; }
+
+        private void OnAddComandExecute(object p)
+        {
+            Number2 = Ariph.Calc(Number1);
+        }
+
+        private bool CanCalcCommandExecuted(object p)
+        {
+            if (Number1 != 0 || Number2 != 0)
+                return true;
+            else return false;
+        }
+       
+        public MainWindowViewModel()
+        {
+            CalcCommand = new RelayCommand(OnAddComandExecute, CanCalcCommandExecuted);
+        }
+    }
+}
